1db65cf99fe21a3d010ab80cac04677a016d7d87
Moiz Qureshi <30798716+imoizuddin@users.noreply.github.com>
2021-09-16 23:29:58 +0530

    showing latitude on screen

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errMessage: err.message });
+      }
     );
-    return <div>Latitude</div>;
+  }
+
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error:{this.state.errMessage}</div>;
+    }
+    if (!this.state.errMessage && this.state.lat) {
+      return <div>Latitude:{this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
