c79ca71ada27a9fe7130190128c077dc99963ed6
Moizuddin Qureshi <imoizuddin@gmail.com>
2021-09-25 08:55:58 +0530

    added icons and bg with loader

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errMessage: "" };
 
-    this.state = { lat: null, errMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errMessage: err.message })
     );
   }
 
@@ -22,9 +18,10 @@ class App extends React.Component {
       return <div>Error:{this.state.errMessage}</div>;
     }
     if (!this.state.errMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat}</div>;
+      // return <div>Latitude:{this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Spinner message="Please accept the location request" />;
   }
 }
 
